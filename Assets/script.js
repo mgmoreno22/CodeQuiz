97411e1a7e9697a02d9511955631a5241db5beea
Michael Moreno <mmoreno2222@gmail.com>
2020-07-25 11:41:10 -0700

    adding initial files

---
+++ Assets/script.js
@@ -0,0 +1,26 @@
+var timeEl = document.getElementById("countdown")
+var questionDiv = document.getElementById("questions")
+var questionEl = document.getElementById("question")
+var answersEl = document.getElementById("answers")
+var startBtn = document.querySelector(".start")
+var submitBtn = document.getElementById("submit")
+var feedbackEl = document.getElementById("feedback")
+
+startBtn.addEventListener("click", function(){
+    // var countdown = prepareCountdown(function() {
+
+    // })
+    var startScreen = document.querySelector(".container")
+    startScreen.setAttribute("class", "hide")
+    questionDiv.removeAttribute("class")
+
+    //Run function to show questions and choices
+        //loop that shows questions based on clicking
+        //capture score
+        
+    
+})
+
+submitBtn.addEventListener("click", function() {
+
+})
